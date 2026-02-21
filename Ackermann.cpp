c1da551169872878ac43459f668cdb2cefcca57a
Aditi Menon <aditimenon@Aditis-MacBook-Pro.local>
2026-02-21 14:23:00 -0800

    Completed

--- Ackermann.cpp
+++ Ackermann.cpp
@@ -4,6 +4,20 @@ using namespace std;
 // Ackermann function definition
 unsigned long long ackermann(unsigned int m, unsigned int n) {
  // TODO: Complete the code.
+    // Case 1: If m == 0
+    if (m == 0) {
+        return n + 1;
+    }
+
+    // Case 2: If m > 0 and n == 0
+    else if (n == 0) {
+        return ackermann(m - 1, 1);
+    }
+
+    // Case 3: If m > 0 and n > 0
+    else {
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
 }
 
 int main() {
